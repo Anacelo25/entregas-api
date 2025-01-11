18d89ecb7c920c7f69b466cdcc3f37c87aedc0a1
Marcelo <marcelogemaque25@gmail.com>
2025-01-11 18:52:48 -0300

    Initial commit

---
+++ src/test/java/com/sistema/entregas_api/EntregasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistema.entregas_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntregasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
